Genymobile/scrcpy

Replace delay by deadline in timedwait()
The function sc_cond_timedwait() accepted a parameter representing the
max duration to wait, because it internally uses SDL_CondWaitTimeout().

Instead, accept a deadline, to be consistent with
pthread_cond_timedwait().

app/src/server.c
@@ -557,7 +557,7 @@ server_stop(struct server *server) {
 #define WATCHDOG_DELAY_MS 1000
         signaled = sc_cond_timedwait(&server->process_terminated_cond,
                                      &server->mutex,
-                                     WATCHDOG_DELAY_MS);
+                                     sc_tick_now() + WATCHDOG_DELAY_MS);
     }
     sc_mutex_unlock(&server->mutex);
 

NiceLeader/Scratchcard-Game

app/src/server.c
@@ -557,7 +557,7 @@ server_stop(struct server *server) {
 #define WATCHDOG_DELAY SC_TICK_FROM_SEC(1)
         signaled = sc_cond_timedwait(&server->process_terminated_cond,
                                      &server->mutex,
-                                     WATCHDOG_DELAY);
+                                     sc_tick_now() + WATCHDOG_DELAY);
     }
     sc_mutex_unlock(&server->mutex);
 