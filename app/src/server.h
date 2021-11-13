Genymobile/scrcpy

Reorder server and server_params
This will allow to define a server_params field in server.

app/src/server.h
@@ -14,23 +14,6 @@
 #include "util/net.h"
 #include "util/thread.h"
 
-struct server {
-    char *serial;
-    process_t process;
-    sc_thread wait_server_thread;
-
-    sc_mutex mutex;
-    sc_cond process_terminated_cond;
-    bool process_terminated;
-
-    sc_socket server_socket; // only used if !tunnel_forward
-    sc_socket video_socket;
-    sc_socket control_socket;
-    uint16_t local_port; // selected from port_range
-    bool tunnel_enabled;
-    bool tunnel_forward; // use "adb forward" instead of "adb reverse"
-};
-
 struct server_params {
     const char *serial;
     enum sc_log_level log_level;
@@ -48,6 +31,23 @@ struct server_params {
     bool stay_awake;
     bool force_adb_forward;
     bool power_off_on_close;
+};
+
+struct server {
+    char *serial;
+    process_t process;
+    sc_thread wait_server_thread;
+
+    sc_mutex mutex;
+    sc_cond process_terminated_cond;
+    bool process_terminated;
+
+    sc_socket server_socket; // only used if !tunnel_forward
+    sc_socket video_socket;
+    sc_socket control_socket;
+    uint16_t local_port; // selected from port_range
+    bool tunnel_enabled;
+    bool tunnel_forward; // use "adb forward" instead of "adb reverse"
 };
 
 // init default values

NiceLeader/Scratchcard-Game

app/src/server.h
@@ -20,21 +20,6 @@ struct server_info {
     struct sc_size frame_size;
 };
 
-struct server {
-    char *serial;
-
-    sc_pid process;
-    // alive only between start() and stop()
-    struct sc_process_observer observer;
-
-    sc_socket server_socket; // only used if !tunnel_forward
-    sc_socket video_socket;
-    sc_socket control_socket;
-    uint16_t local_port; // selected from port_range
-    bool tunnel_enabled;
-    bool tunnel_forward; // use "adb forward" instead of "adb reverse"
-};
-
 struct server_params {
     const char *serial;
     enum sc_log_level log_level;
@@ -52,6 +37,21 @@ struct server_params {
     bool stay_awake;
     bool force_adb_forward;
     bool power_off_on_close;
+};
+
+struct server {
+    char *serial;
+
+    sc_pid process;
+    // alive only between start() and stop()
+    struct sc_process_observer observer;
+
+    sc_socket server_socket; // only used if !tunnel_forward
+    sc_socket video_socket;
+    sc_socket control_socket;
+    uint16_t local_port; // selected from port_range
+    bool tunnel_enabled;
+    bool tunnel_forward; // use "adb forward" instead of "adb reverse"
 };
 
 // init default values